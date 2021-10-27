92a285f1773309ba214b316c4f9b01fa1d0b1ac6
ede1998 <online@erik-hennig.me>
2021-10-27 18:09:11 +0200

    fix cargo deny errors

--- examples/current_user.rs
+++ examples/current_user.rs
@@ -1,4 +1,4 @@
-use simple_logger::SimpleLogger;
+use env_logger::Env;
 use traduora::api::users::Me;
 use traduora::Login;
 use traduora::Query;
@@ -6,7 +6,7 @@ use traduora::Traduora;
 use traduora::TraduoraError;
 
 fn main() -> Result<(), TraduoraError> {
-    SimpleLogger::new().init().unwrap();
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
     let login = Login::password("test@test.test", "12345678");
     let client = Traduora::with_auth_insecure("localhost:8080", login)?;
     let result = Me.query(&client)?;
--- examples/current_user_async.rs
+++ examples/current_user_async.rs
@@ -1,4 +1,4 @@
-use simple_logger::SimpleLogger;
+use env_logger::Env;
 use traduora::api::users::Me;
 use traduora::AsyncQuery;
 use traduora::AsyncTraduora;
@@ -7,7 +7,7 @@ use traduora::TraduoraError;
 
 #[tokio::main]
 async fn main() -> Result<(), TraduoraError> {
-    SimpleLogger::new().init().unwrap();
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
     let login = Login::password("test@test.test", "12345678");
     let client = AsyncTraduora::with_auth_insecure("localhost:8080", login).await?;
     let result = Me.query_async(&client).await?;
